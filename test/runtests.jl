332776ae995f03595985c6cea86c644eb45e0196
TT <tobias.thummerer@informatik.uni-augsburg.de>
2021-05-28 08:10:45 +0200

    (very) basic ME and CS tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,50 +8,6 @@ using FMI
 using Test
 import Random
 
-###############
-# Prepare FMU #
-###############
-
-pathToFMU = joinpath(dirname(@__FILE__), "../model/IO.fmu")
-
-myFMU = fmiLoad(pathToFMU)
-@test fmiInstantiate!(myFMU; loggingOn=false) != 0
-
-@test fmiSetupExperiment(myFMU, 0.0) == 0
-
-@test fmiEnterInitializationMode(myFMU) == 0
-
-#########################
-# Testing Setter/Getter #
-#########################
-
-rndReal = 100 * rand()
-rndInteger = round(Integer, 100 * rand())
-rndBoolean = rand() > 0.5
-rndString = Random.randstring(12)
-
-@test fmiSetReal(myFMU, "p_real", rndReal) == 0
-@test fmiGetReal(myFMU, "p_real") == rndReal
-@test fmiSetReal(myFMU, "p_real", -rndReal) == 0
-@test fmiGetReal(myFMU, "p_real") == -rndReal
-
-@test fmiSetInteger(myFMU, "p_integer", rndInteger) == 0
-@test fmiGetInteger(myFMU, "p_integer") == rndInteger
-@test fmiSetInteger(myFMU, "p_integer", -rndInteger) == 0
-@test fmiGetInteger(myFMU, "p_integer") == -rndInteger
-
-@test fmiSetBoolean(myFMU, "p_boolean", rndBoolean) == 0
-@test fmiGetBoolean(myFMU, "p_boolean") == rndBoolean
-@test fmiSetBoolean(myFMU, "p_boolean", !rndBoolean) == 0
-@test fmiGetBoolean(myFMU, "p_boolean") == !rndBoolean
-
-@test fmiSetString(myFMU, "p_string", rndString) == 0
-@test fmiGetString(myFMU, "p_string") == rndString
-
-############
-# Clean up #
-############
-
-@test fmiExitInitializationMode(myFMU) == 0
-
-fmiUnload(myFMU)
+include("test_setter_getter.jl")
+include("test_sim_cs.jl")
+include("test_sim_me.jl")
