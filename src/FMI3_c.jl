1bd660d2f4b38388dc6d9561267ec5b077ca7ea2
JoKircher <79573182+JoKircher@users.noreply.github.com>
2021-11-04 16:45:20 +0100

    define types and TODOs

--- src/FMI3_c.jl
+++ src/FMI3_c.jl
@@ -2,3 +2,40 @@
 # Copyright (c) 2021 Tobias Thummerer, Lars Mikelsons, Josef Kircher
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
+"""
+Source: FMISpec3.0, Version 5b80c29:2.2.2. Platform Dependent Definitions
+
+To simplify porting, no C types are used in the function interfaces, but the alias types are defined in this section. 
+All definitions in this section are provided in the header file fmi3PlatformTypes.h. It is required to use this definition for all binary FMUs.
+"""
+const fmi3Float32 = Float32
+const fmi3Float64 = Float64
+const fmi3Int8 = Cchar
+const fmi3UInt8 = Cuchar
+const fmi3Int16 = Cshort
+const fmi3UInt16 = Cushort
+const fmi3Int32 = Cint
+const fmi3UInt32 = Cuint
+const fmi3Int64 = Clonglong
+const fmi3UInt64 = Culonglong
+const fmi3Boolean = Cint
+const fmi3Char = Cchar
+const fmi3String = String # TODO: correct it
+const fmi3Byte = Cuchar
+const fmi3Binary = Ptr{fmi3Byte}
+const fmi3ValueReference = Cint
+const fmi3FMUstate = Ptr{Cvoid}
+const fmi3ComponentEnvironment = Ptr{Cvoid}
+const fmi3Enum = Array{Array{String}} # TODO: correct it
+
+# TODO: Callback functions
+
+# TODO: Model description
+
+# TODO: fmi3Component struct
+
+# TODO: 
+# fmi3InstantiateModelExchange
+# fmi3InstantiateCoSimulation
+# fmi3InstantiateSchedualedExecution
+# fmi3 ...
