e2b31ebdd29d9c5997e874438f5d6bee85266bdb
JoKircher <79573182+JoKircher@users.noreply.github.com>
2021-11-04 16:56:45 +0100

    started struct FMU3

--- src/FMI3.jl
+++ src/FMI3.jl
@@ -3,13 +3,36 @@
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
+using Libdl
+using ZipFile
+
+include("FMI3_c.jl")
+include("FMI3_comp.jl")
+include("FMI3_md.jl")
+
 """
 IN PROGRESS:
 The mutable struct representing an FMU in the FMI 3.0 Standard.
 Also contains the paths to the FMU and ZIP folder as well als all the FMI 3.0 function pointers
 """
-mutable struct FMU3
+mutable struct FMU3 <: FMU
+    modelName::fmi3String
+    instanceName::fmi3String
+    fmuResourceLocation::fmi3String
 
+    modelDescription::fmi3ModelDescription
+
+    type::fmi3Type
+    callbackFunctions::fmi3CallbackFunctions
+    components::Array{fmi3Component}
+
+    # paths of ziped and unziped FMU folders
+    path::String
+    zipPath::String
     # Constructor
     fmu3() = new()
+
+    # c-libraries
+    libHandle::Ptr{Nothing}
+
 end
