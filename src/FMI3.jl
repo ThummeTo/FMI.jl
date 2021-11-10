4e99d6aa8cc436c3f5e0ec3905c2cb60c2ab89cb
JoKircher <79573182+JoKircher@users.noreply.github.com>
2021-11-10 10:07:56 +0100

    WIP: starting implementing ModelDescription and FMI3 functions

--- src/FMI3.jl
+++ src/FMI3.jl
@@ -36,3 +36,249 @@ mutable struct FMU3 <: FMU
     libHandle::Ptr{Nothing}
 
 end
+
+
+"""
+Sets the properties of the fmu by reading the modelDescription.xml.
+Retrieves all the pointers of binary functions.
+
+Returns the instance of the FMU struct.
+
+Via optional argument ```unpackPath```, a path to unpack the FMU can be specified (default: system temporary directory).
+"""
+function fmi3Load(pathToFMU::String; unpackPath=nothing)
+    # Create uninitialized FMU
+    fmu = FMU3()
+    fmu.components = []
+
+    pathToFMU = normpath(pathToFMU)
+
+    # set paths for fmu handling
+    (fmu.path, fmu.zipPath) = fmi3Unzip(pathToFMU; unpackPath=unpackPath) # TODO
+
+    # set paths for modelExchangeScripting and binary
+    tmpName = splitpath(fmu.path)
+    pathToModelDescription = joinpath(fmu.path, "modelDescription.xml")
+
+    # parse modelDescription.xml
+    fmu.modelDescription = fmi3ReadModelDescription(pathToModelDescription) # TODO
+    fmu.modelName = fmu.modelDescription.modelName
+    fmu.instanceName = fmu.modelDescription.modelName
+    fmuName = fmi3GetModelIdentifier(fmu.modelDescription) # tmpName[length(tmpName)] TODO
+
+    directoryBinary = ""
+    pathToBinary = ""
+
+    if Sys.iswindows()
+        directories = [joinpath("binaries", "win64"), joinpath("binaries","x86_64-windows")]
+        for directory in directories
+            directoryBinary = joinpath(fmu.path, directory)
+            if isdir(directoryBinary)
+                pathToBinary = joinpath(directoryBinary, "$(fmuName).dll")
+                break
+            end
+        end
+        @assert isfile(pathToBinary) "Target platform is Windows, but can't find valid FMU binary at `$(pathToBinary)` for path `$(fmu.path)`."
+    elseif Sys.islinux()
+        directories = [joinpath("binaries", "linux64"), joinpath("binaries", "x86_64-linux")]
+        for directory in directories
+            directoryBinary = joinpath(fmu.path, directory)
+            if isdir(directoryBinary)
+                pathToBinary = joinpath(directoryBinary, "$(fmuName).so")
+                break
+            end
+        end
+        @assert isfile(pathToBinary) "Target platform is Linux, but can't find valid FMU binary at `$(pathToBinary)` for path `$(fmu.path)`."
+    elseif Sys.isapple()
+        directories = [joinpath("binaries", "darwin64"), joinpath("binaries", "x86_64-darwin")]
+        for directory in directories
+            directoryBinary = joinpath(fmu.path, directory)
+            if isdir(directoryBinary)
+                pathToBinary = joinpath(directoryBinary, "$(fmuName).dylib")
+                break
+            end
+        end
+        @assert isfile(pathToBinary) "Target platform is macOS, but can't find valid FMU binary at `$(pathToBinary)` for path `$(fmu.path)`."
+    else
+        @assert false "Unsupported target platform. Supporting Windows64, Linux64 and Mac64."
+    end
+
+    lastDirectory = pwd()
+    cd(directoryBinary)
+
+    # set FMU binary handler
+    fmu.libHandle = dlopen(pathToBinary)
+
+    cd(lastDirectory)
+
+    if fmi3IsCoSimulation(fmu) # TODO
+        fmu.type = fmi3CoSimulation::fmi3Type
+    elseif fmi3IsModelExchange(fmu) #TODO
+        fmu.type = fmi3ModelExchange::fmi3Type
+    elseif fmi3IsScheduledExecution(fmu) # TODO
+        fmu.type = fmi3ScheduledExecution::fmi3Type
+
+        error(unknownFMUType)
+    end
+
+    if fmi3IsCoSimulation(fmu) && fmi3IsModelExchange(fmu) # TODO
+        @info "fmi3Load(...): FMU supports both CS and ME, using CS as default if nothing specified." # TODO ScheduledExecution
+    end
+
+    # make URI ressource location
+    tmpResourceLocation = string("file:///", fmu.path)
+    tmpResourceLocation = joinpath(tmpResourceLocation, "resources")
+    fmu.fmuResourceLocation = replace(tmpResourceLocation, "\\" => "/") # URIs.escapeuri(tmpResourceLocation)
+
+    @info "fmi3Load(...): FMU resources location is `$(fmu.fmuResourceLocation)`"
+
+    # # retrieve functions TODO check new Names and availability in FMI3
+    # fmu.cInstantiate                  = dlsym(fmu.libHandle, :fmi2Instantiate)
+    # fmu.cGetTypesPlatform             = dlsym(fmu.libHandle, :fmi2GetTypesPlatform)
+    # fmu.cGetVersion                   = dlsym(fmu.libHandle, :fmi2GetVersion)
+    # fmu.cFreeInstance                 = dlsym(fmu.libHandle, :fmi2FreeInstance)
+    # fmu.cSetDebugLogging              = dlsym(fmu.libHandle, :fmi2SetDebugLogging)
+    # fmu.cSetupExperiment              = dlsym(fmu.libHandle, :fmi2SetupExperiment)
+    # fmu.cEnterInitializationMode      = dlsym(fmu.libHandle, :fmi2EnterInitializationMode)
+    # fmu.cExitInitializationMode       = dlsym(fmu.libHandle, :fmi2ExitInitializationMode)
+    # fmu.cTerminate                    = dlsym(fmu.libHandle, :fmi2Terminate)
+    # fmu.cReset                        = dlsym(fmu.libHandle, :fmi2Reset)
+    # fmu.cGetReal                      = dlsym(fmu.libHandle, :fmi2GetReal)
+    # fmu.cSetReal                      = dlsym(fmu.libHandle, :fmi2SetReal)
+    # fmu.cGetInteger                   = dlsym(fmu.libHandle, :fmi2GetInteger)
+    # fmu.cSetInteger                   = dlsym(fmu.libHandle, :fmi2SetInteger)
+    # fmu.cGetBoolean                   = dlsym(fmu.libHandle, :fmi2GetBoolean)
+    # fmu.cSetBoolean                   = dlsym(fmu.libHandle, :fmi2SetBoolean)
+
+    # fmu.cGetString                    = dlsym_opt(fmu.libHandle, :fmi2GetString)
+    # fmu.cSetString                    = dlsym_opt(fmu.libHandle, :fmi2SetString)
+
+    # if fmi2CanGetSetState(fmu)
+    #     fmu.cGetFMUstate                  = dlsym_opt(fmu.libHandle, :fmi2GetFMUstate)
+    #     fmu.cSetFMUstate                  = dlsym_opt(fmu.libHandle, :fmi2SetFMUstate)
+    #     fmu.cFreeFMUstate                 = dlsym_opt(fmu.libHandle, :fmi2FreeFMUstate)
+    # end
+
+    # if fmi2CanSerializeFMUstate(fmu)
+    #     fmu.cSerializedFMUstateSize       = dlsym_opt(fmu.libHandle, :fmi2SerializedFMUstateSize)
+    #     fmu.cSerializeFMUstate            = dlsym_opt(fmu.libHandle, :fmi2SerializeFMUstate)
+    #     fmu.cDeSerializeFMUstate          = dlsym_opt(fmu.libHandle, :fmi2DeSerializeFMUstate)
+    # end
+
+    # if fmi2ProvidesDirectionalDerivative(fmu)
+    #     fmu.cGetDirectionalDerivative     = dlsym_opt(fmu.libHandle, :fmi2GetDirectionalDerivative)
+    # end
+
+    # # CS specific function calls
+    # if fmi2IsCoSimulation(fmu)
+    #     fmu.cSetRealInputDerivatives      = dlsym(fmu.libHandle, :fmi2SetRealInputDerivatives)
+    #     fmu.cGetRealOutputDerivatives     = dlsym(fmu.libHandle, :fmi2GetRealOutputDerivatives)
+    #     fmu.cDoStep                       = dlsym(fmu.libHandle, :fmi2DoStep)
+    #     fmu.cCancelStep                   = dlsym(fmu.libHandle, :fmi2CancelStep)
+    #     fmu.cGetStatus                    = dlsym(fmu.libHandle, :fmi2GetStatus)
+    #     fmu.cGetRealStatus                = dlsym(fmu.libHandle, :fmi2GetRealStatus)
+    #     fmu.cGetIntegerStatus             = dlsym(fmu.libHandle, :fmi2GetIntegerStatus)
+    #     fmu.cGetBooleanStatus             = dlsym(fmu.libHandle, :fmi2GetBooleanStatus)
+    #     fmu.cGetStringStatus              = dlsym(fmu.libHandle, :fmi2GetStringStatus)
+    # end
+
+    # # ME specific function calls
+    # if fmi2IsModelExchange(fmu)
+    #     fmu.cEnterContinuousTimeMode      = dlsym(fmu.libHandle, :fmi2EnterContinuousTimeMode)
+    #     fmu.cGetContinuousStates          = dlsym(fmu.libHandle, :fmi2GetContinuousStates)
+    #     fmu.cGetDerivatives               = dlsym(fmu.libHandle, :fmi2GetDerivatives)
+    #     fmu.cSetTime                      = dlsym(fmu.libHandle, :fmi2SetTime)
+    #     fmu.cSetContinuousStates          = dlsym(fmu.libHandle, :fmi2SetContinuousStates)
+    #     fmu.cCompletedIntegratorStep      = dlsym(fmu.libHandle, :fmi2CompletedIntegratorStep)
+    #     fmu.cEnterEventMode               = dlsym(fmu.libHandle, :fmi2EnterEventMode)
+    #     fmu.cNewDiscreteStates            = dlsym(fmu.libHandle, :fmi2NewDiscreteStates)
+    #     fmu.cGetEventIndicators           = dlsym(fmu.libHandle, :fmi2GetEventIndicators)
+    #     fmu.cGetNominalsOfContinuousStates= dlsym(fmu.libHandle, :fmi2GetNominalsOfContinuousStates)
+    # end
+
+    # # initialize further variables TODO check if needed
+    # fmu.jac_x = zeros(Float64, fmu.modelDescription.numberOfContinuousStates)
+    # fmu.jac_t = -1.0
+    # fmu.jac_dxy_x = zeros(fmi2Real,0,0)
+    # fmu.jac_dxy_u = zeros(fmi2Real,0,0)
+   
+    # dependency matrix 
+    # fmu.dependencies
+
+    fmu
+end
+"""
+Create a copy of the .fmu file as a .zip folder and unzips it.
+Returns the paths to the zipped and unzipped folders.
+
+Via optional argument ```unpackPath```, a path to unpack the FMU can be specified (default: system temporary directory).
+"""
+function fmi3Unzip(pathToFMU::String; unpackPath=nothing)
+
+    fileNameExt = basename(pathToFMU)
+    (fileName, fileExt) = splitext(fileNameExt)
+        
+    if unpackPath == nothing
+        # cleanup=true leads to issues with automatic testing on linux server.
+        unpackPath = mktempdir(; prefix="fmijl_", cleanup=false)
+    end
+
+    zipPath = joinpath(unpackPath, fileName * ".zip")
+    unzippedPath = joinpath(unpackPath, fileName)
+
+    # only copy ZIP if not already there
+    if !isfile(zipPath)
+        cp(pathToFMU, zipPath; force=true)
+    end
+
+    @assert isfile(zipPath) ["fmi3Unzip(...): ZIP-Archive couldn't be copied to `$zipPath`."]
+
+    zipAbsPath = isabspath(zipPath) ?  zipPath : joinpath(pwd(), zipPath)
+    unzippedAbsPath = isabspath(unzippedPath) ? unzippedPath : joinpath(pwd(), unzippedPath)
+
+    @assert isfile(zipAbsPath) ["fmi3Unzip(...): Can't deploy ZIP-Archive at `$(zipAbsPath)`."]
+
+    numFiles = 0
+
+    # only unzip if not already done
+    if !isdir(unzippedAbsPath)
+        mkpath(unzippedAbsPath)
+
+        zarchive = ZipFile.Reader(zipAbsPath)
+        for f in zarchive.files
+            fileAbsPath = normpath(joinpath(unzippedAbsPath, f.name))
+
+            if endswith(f.name,"/") || endswith(f.name,"\\")
+                mkpath(fileAbsPath) # mkdir(fileAbsPath)
+
+                @assert isdir(fileAbsPath) ["fmi2Unzip(...): Can't create directory `$(f.name)` at `$(fileAbsPath)`."]
+            else
+                # create directory if not forced by zip file folder
+                mkpath(dirname(fileAbsPath))
+
+                numBytes = write(fileAbsPath, read(f))
+                
+                if numBytes == 0
+                    @info "fmi2Unzip(...): Written file `$(f.name)`, but file is empty."
+                end
+
+                @assert isfile(fileAbsPath) ["fmi2Unzip(...): Can't unzip file `$(f.name)` at `$(fileAbsPath)`."]
+                numFiles += 1
+            end
+        end
+        close(zarchive)
+    end
+
+    @assert isdir(unzippedAbsPath) ["fmi2Unzip(...): ZIP-Archive couldn't be unzipped at `$(unzippedPath)`."]
+    @info "fmi2Unzip(...): Successfully unzipped $numFiles files at `$unzippedAbsPath`."
+
+    (unzippedAbsPath, zipAbsPath)
+end
+""" 
+Returns how a variable depends on another variable based on the model description.
+"""
+function fmi3VariableDependsOnVariable(fmu::FMU2, vr1::fmi2ValueReference, vr2::fmi2ValueReference) # TODO check what it does
+    i1 = fmu.modelDescription.valueReferenceIndicies[vr1]
+    i2 = fmu.modelDescription.valueReferenceIndicies[vr2]
+    return fmi2GetDependencies(fmu)[i1, i2]
+end
--- src/FMI3_c.jl
+++ src/FMI3_c.jl
@@ -28,14 +28,178 @@ const fmi3FMUstate = Ptr{Cvoid}
 const fmi3ComponentEnvironment = Ptr{Cvoid}
 const fmi3Enum = Array{Array{String}} # TODO: correct it
 
+"""
+Source: FMISpec2.0.2[p.19]: 2.1.5 Creation, Destruction and Logging of FMU Instances
+
+Argument fmuType defines the type of the FMU:
+- fmi2ModelExchange: FMU with initialization and events; between events simulation of continuous systems is performed with external integrators from the environment.
+- fmi2CoSimulation: Black box interface for co-simulation.
+"""
+@enum fmi3Type begin
+    fmi3ModelExchange
+    fmi3CoSimulation
+    fmi3ScheduledExecution
+end
+
 # TODO: Callback functions
 
+
+mutable struct fmi3ModelVariable
+    #mandatory
+    name::fmi2String
+    valueReference::fmi2ValueReference
+    datatype::datatypeVariable
+
+    # Optional
+    description::fmi2String
+    causality::fmi2causality
+    variability::fmi2variability
+    initial::fmi2initial
+
+    # dependencies 
+    dependencies #::Array{fmi2Integer}
+    dependenciesKind #::Array{fmi2String}
+
+    # Constructor for not further specified ScalarVariables
+    function fmi3ModelVariable(name, valueReference)
+        new(name, Cint(valueReference), datatypeVariable(), "", _local::fmi2causality, continuous::fmi2variability, calculated::fmi2initial)
+    end
+
+    # Constructor for fully specified ScalarVariable
+    function fmi3ModelVariable(name, valueReference, datatype, description, causalityString, variabilityString, initialString, dependencies, dependenciesKind)
+
+        var = continuous::fmi2variability
+        cau = _local::fmi2causality
+        init = calculated::fmi2initial
+        #check if causality, variability and initial are correct
+        if !occursin(variabilityString, string(instances(fmi2variability)))
+            display("Error: variability not known")
+        else
+            for i in 0:(length(instances(fmi2variability))-1)
+                if variabilityString == string(fmi2variability(i))
+                    var = fmi2variability(i)
+                end
+            end
+        end
+
+        if !occursin(causalityString, string(instances(fmi2causality)))
+            display("Error: causality not known")
+        else
+            for i in 0:(length(instances(fmi2causality))-1)
+                if causalityString == string(fmi2causality(i))
+                    cau = fmi2causality(i)
+                end
+            end
+        end
+
+        if !occursin(initialString, string(instances(fmi2initial)))
+            display("Error: initial not known")
+        else
+            for i in 0:(length(instances(fmi2initial))-1)
+                if initialString == string(fmi2initial(i))
+                    init = fmi2initial(i)
+                end
+            end
+        end
+        new(name, valueReference, datatype, description, cau, var, init, dependencies, dependenciesKind)
+    end
+end
+
+mutable struct fmi3datatypeVariable
+    # mandatory
+    datatype::Union{Type{fmi2String}, Type{fmi2Real}, Type{fmi2Integer}, Type{fmi2Boolean}, Type{fmi2Enum}}
+    declaredType::fmi2String
+
+    # Optional
+    start::Union{fmi2Integer, fmi2Real, fmi2Boolean, fmi2String, Nothing}
+    min::Union{fmi2Integer, fmi2Real, Nothing}
+    max::Union{fmi2Integer, fmi2Real, Nothing}
+    quantity::Union{fmi2String, Nothing}
+    unit::Union{fmi2String, Nothing}
+    displayUnit::Union{fmi2String, Nothing}
+    relativeQuantity::Union{fmi2Boolean, Nothing}
+    nominal::Union{fmi2Real, Nothing}
+    unbounded::Union{fmi2Boolean, Nothing}
+    derivative::Union{Unsigned, Nothing}
+    reinit::Union{fmi2Boolean, Nothing}
+
+    # additional (not in spec)
+    #unknownIndex::Intger 
+    #dependencies::Array{Intger}
+    #dependenciesValueReferences::Array{fmi2ValueReference}
+
+    # Constructor
+    datatypeVariable() = new()
+end
+
 # TODO: Model description
+mutable struct fmi3ModelDescription
+    # FMI model description
+    fmiVersion::String
+    modelName::String
+    guid::String
+    generationTool::String
+    generationDateAndTime::String
+    variableNamingConvention::String
+    instantiationToken::String
+
+    CSmodelIdentifier::String
+    CScanHandleVariableCommunicationStepSize::Bool
+    CSmaxOutputDerivativeOrder::Int
+    CScanGetAndSetFMUstate::Bool
+    CScanSerializeFMUstate::Bool
+    CSprovidesDirectionalDerivatives::Bool
+    CSproivdesAdjointDerivatives::Bool
+
+    MEmodelIdentifier::String
+    MEcanGetAndSetFMUstate::Bool
+    MEcanSerializeFMUstate::Bool
+    MEprovidesDirectionalDerivatives::Bool
+    MEprovidesAdjointDerivatives::Bool
+
+    SEmodelIdentifier::String
+    SEcanGetAndSetFMUstate::Bool
+    SEcanSerializeFMUstate::Bool
+    SEprovidesDirectionalDerivatives::Bool
+    SEprovidesAdjointDerivatives::Bool
+
+
+    # helpers
+    isCoSimulation::Bool
+    isModelExchange::Bool
+    isScheduledExecution::Bool
+
+    description::String
+
+    # Model variables
+    modelVariables::Array{fmi3ModelVariable,1}
+
+    # additionals
+    valueReferences::Array{fmi3ValueReference}
+
+    inputValueReferences::Array{fmi3ValueReference}
+    outputValueReferences::Array{fmi3ValueReference}
+    stateValueReferences::Array{fmi3ValueReference}
+    derivativeValueReferences::Array{fmi3ValueReference}
+
+    numberOfContinuousStates::Int
+    numberOfEventIndicators::Int
+    enumerations::fmi2Enum
+
+    stringValueReferences
+
+    # Constructor for uninitialized struct
+    fmi3ModelDescription() = new()
+
+    # additional fields (non-FMI-specific)
+    valueReferenceIndicies::Dict{Integer,Integer}
+end
+
 
 # TODO: fmi3Component struct
 
 # TODO: 
 # fmi3InstantiateModelExchange
 # fmi3InstantiateCoSimulation
-# fmi3InstantiateSchedualedExecution
+# fmi3InstantiateScheduledExecution
 # fmi3 ...
