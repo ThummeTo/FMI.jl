6b0a764d6d14cca16c1acbb2782d499c51205883
JoKircher <79573182+JoKircher@users.noreply.github.com>
2021-11-17 17:13:08 +0100

    changes to test fmi3 - will be reverted

--- test/testFMU3.jl
+++ test/testFMU3.jl
@@ -1,2 +1,8 @@
 using FMI
 
+xml = FMI.fmi3ReadModelDescription("model/fmi3/BouncingBall/modelDescription.xml")
+xml = FMI.fmi3ReadModelDescription("model/fmi3/Dahlquist/modelDescription.xml")
+xml = FMI.fmi3ReadModelDescription("model/fmi3/Feedthrough/modelDescription.xml")
+xml = FMI.fmi3ReadModelDescription("model/fmi3/LinearTransform/modelDescription.xml")
+xml = FMI.fmi3ReadModelDescription("model/fmi3/Stair/modelDescription.xml")
+xml = FMI.fmi3ReadModelDescription("model/fmi3/VanDerPol/modelDescription.xml")
