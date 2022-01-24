791a45340e685577def8927d0db099157800a8af
JoKircher <79573182+JoKircher@users.noreply.github.com>
2022-01-24 04:01:53 -0330

    Fmi3 (#75)
    * creating new files
    
    * define types and TODOs
    
    * copy FMI2_md.jl for orientation
    
    * started struct FMU3
    
    * WIP: starting implementing ModelDescription and FMI3 functions
    
    * WIP: more progress
    
    * WIP: Update ModelDescription parsing
    
    * WIP:more changes
    
    * Finish first compilable version of model description
    
    Matrices are not supported yet
    
    * changes to test fmi3 - will be reverted
    
    * instantiated first fmi3 FMU
    
    * more testing
    
    * WIP:started with fmi3InstantiateCoSimulation
    
    * Update testFMU3.jl
    
    * added Getter setter for Int and real datatypes
    
    * implemented all getter and setter
    not all working yet and not all tested
    
    * implemented all FMI functions in FMI3_c.jl
    Partially tested and some more accessable functionscalls missing
    
    * WIP: fmi3DoStep not working
    
    * Enum problem solved
    
    Have to add JSON3 to the dependencies to work
    
    no more warning if variability or causality can't be parsed
    
    * added parsing for Arrays in ModelDescription
    
    * Temporary commit to add JSON3 to project toml
    
    * fixed CallbackFunctions
    
    * Added fmi3SImulateCS and visualization
    
    * implemented fmi3Simulate ME. Memory Error
    
    * WIP: simulate ME
    
    * WIP: ME simulation running
    
    * Added documentation and missing functions.
    
    * WIP: event handling kinda works, rest not so well
    
    * to be deleted
    
    * Update the simulate functions fmi3
    
    * WIP: gettin there
    
    * WIP: fixed fmi3GetBoolean
    
    * alle CCalls gecheckt
    
    fast alle funktionieren
    
    * notizen
    
    * Update docstrings
    
    * Sim
    
    * Update anmerkungen.md
    
    * Update README.md
    
    * WIP: es geht dem ENde zu
    
    * WIP
    
    * Update FMI3_sim.jl
    
    * Update FMI3_sim.jl
    
    * More work done
    
    * Docstrings und vorletzte Politur
    
    * Added Docstring tags for comp and FMI3
    
    * More changes
    
    * binary update
    
    * WIP: finishing touches
    
    * fixed Event handling so it works
    
    * Refactor and polish
    
    * Bug-fix (saving callback, improoved error messages)
    
    * delete redundant code and printlns
    
    * remove simulation result
    
    * Delete unnecessary files
    
    * final commit
    
    Co-authored-by: TT <tobias.thummerer@informatik.uni-augsburg.de>
    Co-authored-by: ThummeTo <83663542+ThummeTo@users.noreply.github.com>

---
+++ src/FMI3_sim.jl
@@ -0,0 +1,463 @@
+#
+# Copyright (c) 2021 Tobias Thummerer, Lars Mikelsons, Josef Kircher
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
+using DifferentialEquations, DiffEqCallbacks
+
+############ Model-Exchange ############
+
+# Read next time event from fmu and provide it to the integrator 
+function time_choice(c::fmi3Component, integrator,) 
+    @debug "time_choice: Time Event ", timeEvent[]
+
+    discreteStatesNeedUpdate, terminateSimulation, nominalsOfContinuousStatesChanged, valuesOfContinuousStatesChanged, nextEventTimeDefined, nextEventTime = fmi3UpdateDiscreteStates(c)
+    fmi3EnterContinuousTimeMode(c)
+    if Bool(nextEventTimeDefined)
+        c.timeEvent = integrator.t >= nextEventTime
+        nextEventTime
+    else
+        Inf
+    end
+end
+
+# Handles events and returns the values and nominals of the changed continuous states.
+function handleEvents(c::fmi3Component, enterEventMode::Bool, exitInContinuousMode::Bool)
+    nominalsChanged = fmi3False
+    valuesChanged = fmi3False
+    if enterEventMode
+        correct_step = false
+        indicators = fmi3GetEventIndicators(c)
+        for i in 1:length(c.rootsFound)
+            if c.rootsFound[i] != 0
+                if c.rootsFound[i] != sign(indicators[i])
+                    # we are at left roots
+                    correct_step = true
+                end
+            end
+        end
+        if correct_step
+            x = fmi3GetContinuousStates(c)
+            dx = fmi3GetContinuousStateDerivatives(c)
+            newx = x + dx * 1e-12
+            fmi3SetContinuousStates(c, newx)
+            # integrator.u = newx
+            # integrator.t += 1e-12
+            @debug "set x from $x to $newx"
+        end
+
+        fmi3EnterEventMode(c, c.stepEvent, c.stateEvent, c.rootsFound, Csize_t(c.fmu.modelDescription.numberOfEventIndicators), c.timeEvent)
+        # TODO inputEvent handling
+        
+        discreteStatesNeedUpdate = fmi3True
+        while discreteStatesNeedUpdate == fmi3True
+
+            # update discrete states
+            discreteStatesNeedUpdate, terminateSimulation, nominalsOfContinuousStatesChanged, valuesOfContinuousStatesChanged, nextEventTimeDefined, nextEventTime = fmi3UpdateDiscreteStates(c)
+            nominalsChanged |= nominalsOfContinuousStatesChanged
+            valuesChanged |= valuesOfContinuousStatesChanged
+            if terminateSimulation == fmi3True
+                @error "fmi3UpdateDiscreteStates returned error!"
+            end
+        end
+    end
+    
+
+    if exitInContinuousMode
+        fmi3EnterContinuousTimeMode(c)
+    end
+    @debug "handleEvents: rootsFound: $(c.rootsFound)        enterEventMode: $enterEventMode   valuesChanged: $valuesChanged     continuousStates: ", fmi3GetContinuousStates(c)
+    return valuesChanged, nominalsChanged
+
+end
+
+# Returns the event indicators for an FMU.
+function condition(c::fmi3Component, out, x, t, integrator, inputFunction, inputValues::Array{fmi3ValueReference}) # Event when event_f(u,t) == 0
+    if inputFunction !== nothing
+        fmi3SetFloat64(c, inputValues, inputFunction(integrator.t))
+    end
+    c.stateEvent = fmi3False
+    fmi3SetTime(c, t)
+    fmi3SetContinuousStates(c, x)
+    indicators = fmi3GetEventIndicators(c)
+    if length(indicators) > 0
+        for i in 1:length(indicators)
+            if c.previous_z[i] < 0 && indicators[i] >= 0
+                c.rootsFound[i] = 1
+            elseif c.previous_z[i] > 0 && indicators[i] <= 0
+                c.rootsFound[i] = -1
+            else
+                c.rootsFound[i] = 0
+            end
+            c.stateEvent |= (c.rootsFound[i] != 0)
+            # c.previous_z[i] = indicators[i]
+        end
+    end
+    @debug "condition: eventIndicators $indicators      t $t       rootsFound $(c.rootsFound)      stateEvent $(c.stateEvent)      continuousStates ", fmi3GetContinuousStates(c)
+    copy!(out, indicators)
+end
+
+# Handles the upcoming events.
+function affectFMU!(c::fmi3Component, integrator, idx, inputFunction, inputValues::Array{fmi3ValueReference}, force=false)
+    # Event found - handle it
+    continuousStatesChanged, nominalsChanged = handleEvents(c, true, Bool(sign(idx)))
+
+    @debug "AffectFMU!: continuousStatesChanged " ,  continuousStatesChanged
+    if inputFunction !== nothing
+        fmi3SetFloat64(c, inputValues, inputFunction(integrator.t))
+    end
+
+    if continuousStatesChanged == fmi3True
+        integrator.u = fmi3GetContinuousStates(c)
+        @debug "set new state! ", integrator.u
+    end
+
+    if nominalsChanged == fmi3True
+        x_nom = fmi3GetNominalsOfContinuousStates(c)
+    end
+    # timeEventCb = PresetTimeCallback(2.0, (integrator) -> affect!(c, integrator, 0))
+end
+
+# Does one step in the simulation.
+function stepCompleted(c::fmi3Component, x, t, integrator, inputFunction, inputValues::Array{fmi3ValueReference})
+
+    fmi3SetContinuousStates(c, x)
+    
+    indicators = fmi3GetEventIndicators(c)
+        if length(indicators) > 0
+        c.stateEvent = fmi3False
+    
+        for i in 1:length(indicators)
+            if c.previous_z[i] < 0 && indicators[i] >= 0
+                c.rootsFound[i] = 1
+            elseif c.previous_z[i] > 0 && indicators[i] <= 0
+                c.rootsFound[i] = -1
+            else
+                c.rootsFound[i] = 0
+            end
+            c.stateEvent |= (c.rootsFound[i] != 0)
+            c.previous_z[i] = indicators[i]
+        end
+    end
+
+    (status, c.stepEvent, terminateSimulation) = fmi3CompletedIntegratorStep(c, fmi3True)
+    @debug "stepCompleted: stepEvent $(c.stepEvent)        t $t"
+    @assert terminateSimulation == fmi3False "completed Integratorstep failed!"
+    # if stepEvent == fmi3True
+    #     affectFMU!(c, integrator, 0, inputFunction, inputValues)
+    # else
+    #     if inputFunction !== nothing
+    #         fmi3SetFloat64(c, inputValues, inputFunction(integrator.t))
+    #     end
+    # end
+end
+
+# Returns the state derivatives of the FMU.
+function fx(c::fmi3Component, x, p, t)
+    @debug "fx: t: $t      x:$x"
+    fmi3SetTime(c, t) 
+    fmi3SetContinuousStates(c, x)
+    dx = fmi3GetContinuousStateDerivatives(c)
+end
+
+# save FMU values 
+function saveValues(c::fmi3Component, recordValues, u, t, integrator)
+    (fmi3GetFloat64(c, recordValues)...,)
+end
+
+"""
+Source: FMISpec3.0, Version D5ef1c1: 3.3. Code Example
+
+Simulates a FMU instance for the given simulation time interval.
+State- and Time-Events are handled correctly.
+
+Returns a tuple of type (ODESolution, DiffEqCallbacks.SavedValues).
+If keyword `recordValues` is not set, a tuple of type (ODESolution, nothing) is returned for consitency.
+"""
+function fmi3SimulateME(c::fmi3Component, t_start::Real = 0.0, t_stop::Real = 1.0;
+    solver = nothing,
+    customFx = nothing,
+    recordValues::fmi3ValueReferenceFormat = nothing,
+    saveat = [],
+    setup = true,
+    reset = nothing, # nothing = auto
+    inputValues::fmi3ValueReferenceFormat = nothing,
+    inputFunction = nothing,
+    kwargs...)
+
+    recordValues = prepareValueReference(c, recordValues)
+    inputValues = prepareValueReference(c, inputValues)
+    solution = nothing
+    callbacks = []
+    savedValues = nothing
+
+    savingValues = (length(recordValues) > 0)
+    hasInputs = (length(inputValues) > 0)
+
+    if savingValues
+        savedValues = SavedValues(Float64, Tuple{collect(Float64 for i in 1:length(recordValues))...})
+
+        savingCB = SavingCallback((u,t,integrator) -> saveValues(c, recordValues, u, t, integrator), 
+                                  savedValues, 
+                                  saveat=saveat)
+        push!(callbacks, savingCB)
+    end
+
+    if solver === nothing
+        solver = Tsit5()
+    end
+
+    # auto correct reset if only setup is given
+    if reset === nothing 
+        reset = setup
+    end
+    @assert !(setup==false && reset==true) "fmi3SimulateME(...): keyword argument `setup=false`, but `reset=true`. This may cause a FMU crash."
+
+
+    if reset
+        fmi3Reset(c)
+    end
+
+    if setup
+        fmi3EnterInitializationMode(c, t_start, t_stop)
+        fmi3ExitInitializationMode(c)
+    end
+
+    eventHandling = c.fmu.modelDescription.numberOfEventIndicators > 0 
+    timeEventHandling = false
+    
+    if eventHandling
+        discreteStatesNeedUpdate = fmi3True
+        while discreteStatesNeedUpdate == fmi3True
+            discreteStatesNeedUpdate, terminateSimulation, nominalsOfContinuousStatesChanged, valuesOfContinuousStatesChanged, nextEventTimeDefined, nextEventTime = fmi3UpdateDiscreteStates(c)
+            @assert terminateSimulation == fmi3False ["Initial Event handling failed!"]
+            timeEventHandling |= (nextEventTimeDefined == fmi3True)
+        end  
+    end
+    
+    if customFx === nothing
+        customFx = (x, p, t) -> fx(c, x, p, t)
+    end
+
+    fmi3EnterContinuousTimeMode(c)
+
+    if eventHandling
+        c.previous_z = fmi3GetEventIndicators(c)
+    end
+
+    # First evaluation of the FMU
+    x0 = fmi3GetContinuousStates(c)
+    x0_nom = fmi3GetNominalsOfContinuousStates(c)
+
+    p = []
+    problem = ODEProblem(customFx, x0, (t_start, t_stop), p,)
+    
+    # callback functions
+
+    # use step callback always if we have inputs or need evenet handling
+    if hasInputs || eventHandling
+        stepCb = FunctionCallingCallback((x, t, integrator) -> stepCompleted(c, x, t, integrator, inputFunction, inputValues);
+                                            func_everystep = true,
+                                            func_start = true)
+        push!(callbacks, stepCb)
+    end
+
+    if eventHandling
+
+        eventCb = VectorContinuousCallback((out, x, t, integrator) -> condition(c, out, x, t, integrator, inputFunction, inputValues),
+                                           (integrator, idx) -> affectFMU!(c, integrator, idx, inputFunction, inputValues, true),
+                                           Int64(c.fmu.modelDescription.numberOfEventIndicators);
+                                           rootfind = RightRootFind,
+                                           save_positions=(false,false))
+        push!(callbacks, eventCb)
+
+        if timeEventHandling
+            timeEventCb = IterativeCallback((integrator) -> time_choice(c, integrator),
+                                            (integrator) -> affectFMU!(c, integrator, 0, inputFunction, inputValues), Float64; 
+                                            initial_affect = true,
+                                            save_positions=(false,false))
+            push!(callbacks, timeEventCb)
+        end
+    end
+
+    solution = solve(problem, solver; callback = CallbackSet(callbacks...), saveat = saveat, kwargs...)
+
+    if savingValues
+        return solution, savedValues
+    else 
+        return solution
+    end
+end
+
+"""
+Source: FMISpec3.0, Version D5ef1c1: 4.3. Code Examples
+
+Starts a simulation of the Co-Simulation FMU instance.
+
+Returns a tuple of (success::Bool, DiffEqCallbacks.SavedValues) with success = `true` or `false`.
+If keyword `recordValues` is not set, a tuple of type (success::Bool, nothing) is returned for consitency.
+
+ToDo: Improve Documentation.
+"""
+function fmi3SimulateCS(c::fmi3Component, t_start::Real, t_stop::Real;
+                        recordValues::fmi3ValueReferenceFormat = nothing,
+                        saveat = [],
+                        setup::Bool = true,
+                        reset = nothing,
+                        inputValues::fmi3ValueReferenceFormat = nothing,
+                        inputFunction = nothing)
+
+    recordValues = prepareValueReference(c, recordValues)
+    inputValues = prepareValueReference(c, inputValues)
+    variableSteps = c.fmu.modelDescription.CScanHandleVariableCommunicationStepSize 
+
+    success = false
+    savedValues = nothing
+
+    # default setup
+    if length(saveat) == 0
+        saveat = LinRange(t_start, t_stop, 100)
+    end
+    dt = (t_stop - t_start) / length(saveat)
+
+    # setup if no variable steps
+    if variableSteps == false 
+        if length(saveat) >= 2 
+            dt = saveat[2] - saveat[1]
+        end
+    end
+
+    # auto correct reset if only setup is given
+    if reset === nothing 
+        reset = setup
+    end
+    @assert !(setup==false && reset==true) "fmi3SimulateME(...): keyword argument `setup=false`, but `reset=true`. This may cause a FMU crash."
+
+
+    if reset
+        fmi3Reset(c)
+    end
+
+    if setup
+        fmi3EnterInitializationMode(c, t_start, t_stop)
+        fmi3ExitInitializationMode(c)
+    end
+
+    t = t_start
+
+    record = length(recordValues) > 0
+
+    #numDigits = length(string(round(Integer, 1/dt)))
+    noSetFMUStatePriorToCurrentPoint = fmi3False
+    eventEncountered = fmi3False
+    terminateSimulation = fmi3False
+    earlyReturn = fmi3False
+    lastSuccessfulTime = fmi3Float64(0.0)
+
+    if record
+        savedValues = SavedValues(Float64, Tuple{collect(Float64 for i in 1:length(recordValues))...})
+
+        i = 1
+
+        values = (fmi3GetFloat64(c, recordValues)...,)
+        DiffEqCallbacks.copyat_or_push!(savedValues.t, i, t)
+        DiffEqCallbacks.copyat_or_push!(savedValues.saveval, i, values, Val{false})
+
+        while t < t_stop
+            if variableSteps
+                if length(saveat) > i
+                    dt = saveat[i+1] - saveat[i]
+                else 
+                    dt = t_stop - saveat[i]
+                end
+            end
+
+            if inputFunction !== nothing
+                fmi3SetFloat64(c, inputValues, inputFunction(t))
+            end
+
+            fmi3DoStep(c, t, dt, fmi3True, eventEncountered, terminateSimulation, earlyReturn, lastSuccessfulTime)
+            if eventEncountered == fmi3True
+                @warn "Event handling"
+            end
+            if terminateSimulation == fmi3True
+                @error "fmi3DoStep returned error!"
+            end
+            if earlyReturn == fmi3True
+                @warn "early Return"
+            end
+            t = t + dt #round(t + dt, digits=numDigits)
+            i += 1
+
+            values = (fmi3GetFloat64(c, recordValues)...,)
+            DiffEqCallbacks.copyat_or_push!(savedValues.t, i, t)
+            DiffEqCallbacks.copyat_or_push!(savedValues.saveval, i, values, Val{false})
+        end
+
+        success = true
+        return success, savedValues
+    else
+        i = 1
+        while t < t_stop
+            if variableSteps
+                if length(saveat) > i
+                    dt = saveat[i+1] - saveat[i]
+                else 
+                    dt = t_stop - saveat[i]
+                end
+            end
+
+            if inputFunction !== nothing
+                fmi3SetFloat64(c, inputValues, inputFunction(t))
+            end
+
+            fmi3DoStep(c, t, dt, fmi3True, eventEncountered, terminateSimulation, earlyReturn, lastSuccessfulTime)
+            if eventEncountered == fmi3True
+                @warn "Event handling"
+            end
+            if terminateSimulation == fmi3True
+                @error "fmi3DoStep returned error!"
+            end
+            if earlyReturn == fmi3True
+                @warn "early Return"
+            end
+            t = t + dt #round(t + dt, digits=numDigits)
+            i += 1
+        end
+
+        success = true
+        return success
+    end
+end
+
+# TODO simulate ScheduledExecution
+function fmi3SimulateSE(c::fmi3Component, t_start::Real, t_stop::Real;
+    recordValues::fmi3ValueReferenceFormat = nothing,
+    saveat = [],
+    setup::Bool = true,
+    reset = nothing,
+    inputValues::fmi3ValueReferenceFormat = nothing,
+    inputFunction = nothing)
+    @assert false "Not implemented"
+end
+
+"""
+Starts a simulation of the fmu instance for the matching fmu type, if both types are available, CS is preferred.
+
+Returns:
+    - a tuple of (success::Bool, DiffEqCallbacks.SavedValues) with success = `true` or `false` for CS-FMUs
+    - a tuple of (ODESolution, DiffEqCallbacks.SavedValues) for ME-FMUs
+    - if keyword `recordValues` is not set, a tuple of type (..., nothing)
+    
+ToDo: Improve Documentation.
+"""
+function fmi3Simulate(c::fmi3Component, t_start::Real = 0.0, t_stop::Real = 1.0;kwargs...)
+
+    if fmi3IsCoSimulation(c.fmu)
+        return fmi3SimulateCS(c, t_start, t_stop; kwargs...)
+    elseif fmi3IsModelExchange(c.fmu)
+        return fmi3SimulateME(c, t_start, t_stop; kwargs...)
+    else
+        error(unknownFMUType)
+    end
+end
