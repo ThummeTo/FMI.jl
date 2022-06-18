1b2ac265016d8407d6283842d65309260edfec2e
JoKircher <josef_kircher@gmx.de>
2022-06-18 18:19:21 +0200

    update tests for FMI3

---
+++ test/FMI3/cs_me.jl
@@ -0,0 +1,57 @@
+###############
+# Prepare FMU #
+###############
+
+t_start = 0.0
+t_stop = 1.0
+
+myFMU = fmiLoad("SpringPendulum1D", ENV["EXPORTINGTOOL"], ENV["EXPORTINGVERSION"])
+@test fmiIsCoSimulation(myFMU)
+@test fmiIsModelExchange(myFMU)
+comp = fmiInstantiate!(myFMU; loggingOn=false)
+@test comp != 0
+# choose FMU or FMUComponent
+fmuStruct = nothing
+envFMUSTRUCT = ENV["FMUSTRUCT"]
+if envFMUSTRUCT == "FMU"
+    fmuStruct = myFMU
+elseif envFMUSTRUCT == "FMUCOMPONENT"
+    fmuStruct = comp
+end
+sol = fmiSimulateCS(fmuStruct, t_start, t_stop)
+@test sol.success 
+sol = fmiSimulateME(fmuStruct, t_start, t_stop)
+@test sol.success 
+fmiUnload(myFMU)
+
+
+
+myFMU = fmiLoad("SpringPendulum1D", ENV["EXPORTINGTOOL"], ENV["EXPORTINGVERSION"]; type=:ME)
+@test myFMU.type == FMI.fmi2TypeModelExchange
+comp = fmiInstantiate!(myFMU; loggingOn=false)
+fmuStruct = nothing
+envFMUSTRUCT = ENV["FMUSTRUCT"]
+if envFMUSTRUCT == "FMU"
+    fmuStruct = myFMU
+elseif envFMUSTRUCT == "FMUCOMPONENT"
+    fmuStruct = comp
+end
+sol = fmiSimulate(fmuStruct, t_start, t_stop)
+@test sol.success
+fmiUnload(myFMU)
+
+
+
+myFMU = fmiLoad("SpringPendulum1D", ENV["EXPORTINGTOOL"], ENV["EXPORTINGVERSION"]; type=:CS)
+@test myFMU.type == FMI.fmi2TypeCoSimulation
+comp = fmiInstantiate!(myFMU; loggingOn=false)
+fmuStruct = nothing
+envFMUSTRUCT = ENV["FMUSTRUCT"]
+if envFMUSTRUCT == "FMU"
+    fmuStruct = myFMU
+elseif envFMUSTRUCT == "FMUCOMPONENT"
+    fmuStruct = comp
+end
+sol = fmiSimulate(fmuStruct, t_start, t_stop)
+@test sol.success
+fmiUnload(myFMU)
