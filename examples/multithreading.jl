a900adc641187adf9ded4496023e4c44df614338
Johannes Stoljar <93336654+stoljarjo@users.noreply.github.com>
2022-06-28 10:16:38 +0200

    Examples (#130)
    * Initial commit on orphan branch
    
    * Add all examples
    
    * Add examples action
    
    * Update action
    
    * Bug fix action
    
    * Correct project path
    
    * Change folder name
    
    * Update action
    
    * Checkout current branch
    
    * Testing action
    
    * Testing
    
    * Testing action
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing
    
    * Remove keyword externalCallbacks
    
    * Run examples on windows
    
    * Modify auto commit step
    
    * Jupyter nbconvert synch - modified, paired .ipynb files
    
    * Execute action for windows and linux
    
    * Bug fix, correct issue with same pointer
    
    * Testing action only on windows
    
    * Move action to main
    
    * Add gitignore
    
    * Bug fix parameterize example
    
    * Jupyter nbconvert synch - modified, paired .ipynb files
    
    * Jupyter nbconvert synch - modified, paired .ipynb files
    
    * Update .gitignore
    
    * Jupyter nbconvert synch - modified, paired .ipynb files
    
    * Combine .gitignore
    
    * Move readme to the root
    
    * Update paths to .ipynb .jl .md
    
    * Jupyter nbconvert synch - modified, paired .ipynb files
    
    * Jupyter nbconvert synch - modified, paired .ipynb files
    
    * Jupyter nbconvert synch - modified, paired .ipynb files
    
    * Jupyter nbconvert synch - modified, paired .ipynb files
    
    * Add Manifest.toml to gitignore
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: stoljarjo <stoljarjo@users.noreply.github.com>

---
+++ examples/multithreading.jl
@@ -0,0 +1,42 @@
+# imports
+using FMI
+using FMIZoo
+using Folds
+using BenchmarkTools
+
+Threads.nthreads()
+
+t_start = 0.0
+t_step = 0.1
+t_stop = 10.0
+tspan = (t_start, t_stop)
+tData = collect(t_start:t_step:t_stop)
+
+# Best if batchSize is a multiple of the threads/cores
+batchSize = Threads.nthreads()
+
+# Define an array of arrays randomly
+input_values = collect(collect.(eachrow(rand(batchSize,2))))
+
+
+# a single FMU to compare the performance
+realFMU = fmiLoad("SpringPendulum1D", "Dymola", "2022x")
+
+# the FMU batch
+realFMUBatch = [fmiLoad("SpringPendulum1D", "Dymola", "2022x") for _ in 1:batchSize]
+
+function runCalcFormatted(fmu::FMU2, x0::Vector{Float64}, recordValues::Vector{String}=["mass.s", "mass.v"])
+    data = fmiSimulateME(fmu, t_start, t_stop; recordValues=recordValues, saveat=tData, x0=x0, showProgress=false, dtmax=1e-4)
+    return reduce(hcat, data.states.u)
+end
+
+@benchmark data = runCalcFormatted(realFMU, rand(2))
+
+println("Single Threaded")
+@benchmark collect(runCalcFormatted(realFMU, i) for i in input_values)
+
+println("Multi Threaded")
+@benchmark Folds.collect(runCalcFormatted(fmu, i) for (fmu, i) in zip(realFMUBatch, input_values))
+
+fmiUnload(realFMU)
+fmiUnload.(realFMUBatch)
