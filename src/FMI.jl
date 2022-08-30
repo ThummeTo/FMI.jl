98a4a1352ab715dfe5a6ea87cf6427c775e695c9
adribrune <adrianb.brune@gmail.com>
2022-08-30 07:31:31 +0200

    Updated

--- src/FMI.jl
+++ src/FMI.jl
@@ -1071,7 +1071,7 @@ Sets the n-th time derivative of real input variables.
 More detailed: `fmi2Struct = Union{FMU2, FMU2Component}`
  - `str::FMU2`: Mutable struct representing a FMU and all it instantiated instances in the FMI 2.0.2 Standard.
  - `str::FMU2Component`: Mutable struct represents an instantiated instance of an FMU in the FMI 2.0.2 Standard.
-- `vr::Array{fmi2ValueReference}`: Argument `vr` is an array of `nvr` value handels called "ValueReference" that t define the variables whose derivatives shall be set.
+- `vr::Array{fmi2ValueReference}`: Argument `vr` is an array of `nvr` value handels called "ValueReference" that define the variables whose derivatives shall be set.
 - `order::Array{fmi2Integer}`: Argument `order` is an array of fmi2Integer values witch specifys the corresponding order of derivative of the real input variable.
 -
 
