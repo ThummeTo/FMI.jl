cb557a50829c8806a30cc3149467e2c2aff7f71d
Christof Baumgartner <Christof.baumgartner.cb@gmail.com>
2023-01-14 08:26:22 +0100

    Add changes for new fmiimport version

--- cross_checks/cross_check.jl
+++ cross_checks/cross_check.jl
@@ -118,17 +118,18 @@ function runCrossCheckFmu(checkPath::String, resultPath::String, check::FmuCross
             
             if hasInputValues
                 if check.type == CS
-                    simData = fmiSimulateCS(fmuToCheck, tStart, tStop; tolerance=relTol, saveat=fmuRefValues[1], inputFunction=getInputValues, inputValueReferences=:inputs, recordValues=fmuRecordValueNames)
+                    # simData = fmiSimulateCS(fmuToCheck, (tStart, tStop); tolerance=relTol, saveat=fmuRefValues[1], inputFunction=getInputValues, inputValueReferences=:inputs, recordValues=fmuRecordValueNames)
+                    simData = fmiSimulateCS(fmuToCheck, (tStart, tStop); tolerance=relTol, saveat=fmuRefValues[1], inputFunction=getInputValues, inputValueReferences=:inputs, recordValues=fmuRecordValueNames)
                 elseif check.type == ME
-                    simData = fmiSimulateME(fmuToCheck, tStart, tStop; reltol=relTol, saveat=fmuRefValues[1], inputFunction=getInputValues, inputValueReferences=:inputs, recordValues=fmuRecordValueNames)
+                    simData = fmiSimulateME(fmuToCheck, (tStart, tStop); reltol=relTol, saveat=fmuRefValues[1], inputFunction=getInputValues, inputValueReferences=:inputs, recordValues=fmuRecordValueNames)
                 else
                     @error "Unkown FMU Type. Only 'cs' and 'me' are valid types"
                 end
             else
                 if check.type == CS
-                    simData = fmiSimulateCS(fmuToCheck, tStart, tStop; tolerance=relTol, saveat=fmuRefValues[1], recordValues=fmuRecordValueNames)
+                    simData = fmiSimulateCS(fmuToCheck, (tStart, tStop); tolerance=relTol, saveat=fmuRefValues[1], recordValues=fmuRecordValueNames)
                 elseif check.type == ME
-                    simData = fmiSimulateME(fmuToCheck, tStart, tStop; reltol=relTol, saveat=fmuRefValues[1], recordValues=fmuRecordValueNames)
+                    simData = fmiSimulateME(fmuToCheck, (tStart, tStop); reltol=relTol, saveat=fmuRefValues[1], recordValues=fmuRecordValueNames)
                 else
                     @error "Unkown FMU Type. Only 'cs' and 'me' are valid types"
                 end
