b814ee406c823fcca55451ef01a79a882ed91e5f
TT <tobias.thummerer@informatik.uni-augsburg.de>
2023-11-21 15:36:58 +0100

    fixed version test in performance test

--- test/runtests.jl
+++ test/runtests.jl
@@ -80,7 +80,7 @@ function runtestsFMI2(exportingTool)
             end
         end
 
-        if VERSION >= v"1.7.0"
+        if VERSION >= v"1"
             @info "Performance (performance.jl)"
             @testset "Performance" begin
                 include("FMI2/performance.jl")
