eb8659c5b99501d0478505fb41be39b31ac878a0
ThummeTo <tobias.thummerer@informatik.uni-augsburg.de>
2026-06-29 17:49:08 +0200

    removed simulation code

--- test/sim_SE.jl
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2021 Tobias Thummerer, Lars Mikelsons, Josef Kircher
-# Licensed under the MIT license. See LICENSE file in the project root for details.
-#
-
-# testing different modes for SE (scheduled execution) mode
-
-# [ToDo] coming soon
